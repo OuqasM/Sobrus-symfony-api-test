287cbc2167ab755bd03220d8f9a006bab918b3fe
MahdiOuqas <elmahdi.ouqas@cod.network>
2024-10-30 11:49:48 +0100

    Implement image Upload with file vich uploader bundle

--- sobrus-test/src/Entity/Blog.php
+++ sobrus-test/src/Entity/Blog.php
@@ -4,19 +4,32 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
 use App\Entity\Traits\CommonDate;
 use App\Repository\BlogRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: BlogRepository::class)]
-#[ApiResource()] 
-// #[Get()]
-// #[Post()]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+)] 
+#[Get()]
+#[GetCollection()]
+#[Post(
+    denormalizationContext: ['groups' => ['write']],
+    inputFormats: ['multipart' => 'multipart/form-data']
+)]
 #[HasLifecycleCallbacks]
+#[Vich\Uploadable]
 class Blog
 {
     use CommonDate;
@@ -27,6 +40,7 @@ class Blog
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['write'])]
     private ?string $title = null;
 
     #[ORM\Column(length: 255, nullable: true)]
@@ -37,14 +51,43 @@ class Blog
         minMessage: "You must enter at least {{ limit }} characters",
         maxMessage: "You cannot enter more than {{ limit }} characters",
         )]
+    #[Groups(['write'])]
     private ?string $author = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[Groups(['write'])]
     private ?\DateTimeInterface $pubDate = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     #[Assert\NotBlank]
+    #[Groups(['write','read'])]
     private ?string $text = null;
+
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?int $userId = null;
+
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'blog')]
+    #[Groups(['read'])]
+    private Collection $comments;
+
+    #[Vich\UploadableField(mapping: 'blogs', fileNameProperty: 'imageName', size: 'imageSize')]
+    #[Groups(['write'])]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -97,5 +140,83 @@ class Blog
         $this->text = $text;
 
         return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setBlog($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getBlog() === $this) {
+                $comment->setBlog(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 }
---
+++ sobrus-test/src/Entity/Comment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ApiResource]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['read'])]
+    private ?string $text = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    private ?Blog $blog = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getBlog(): ?Blog
+    {
+        return $this->blog;
+    }
+
+    public function setBlog(?Blog $blog): static
+    {
+        $this->blog = $blog;
+
+        return $this;
+    }
+}
